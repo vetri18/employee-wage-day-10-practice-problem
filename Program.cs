1c87ccb4477b4ce4d48fab6a4fd20eb42b4a0da0
vetri18 <109862568+vetri18@users.noreply.github.com>
2022-08-14 10:03:42 +0530

    using switch month uc 5

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿namespace EmployeeWage
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employeewage program.");
+
+            UsingSwitchMonth usingSwitch = new UsingSwitchMonth();
+            usingSwitch.CheckEmployeeAttendence();
+        }
+    }
+}
