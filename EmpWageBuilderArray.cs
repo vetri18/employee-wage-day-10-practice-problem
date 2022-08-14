b318d4557f58923c3cf2b898dd32ef5f54cd0d12
vetri18 <109862568+vetri18@users.noreply.github.com>
2022-08-14 10:14:27 +0530

    uc 14

---
+++ EmpWageBuilderArray.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWage
+{
+    public class EmpWageBuilderArray : IEmployee
+    {
+        public const int IS_PART_TIME = 1;
+        public const int IS_FUL_TIME = 2;
+
+        private LinkedList<CompanyEmpWage> companyEmpWageList;
+        private Dictionary<string, CompanyEmpWage> companyToEmpWageMap;
+
+        public EmpWageBuilderArray()
+        {
+            this.companyEmpWageList = new LinkedList<CompanyEmpWage>();
+            this.companyToEmpWageMap = new Dictionary<string, CompanyEmpWage>();
+        }
+
+        public void addCompanyEmpWage(string company, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMonth)
+        {
+            CompanyEmpWage companyEmpWage = new CompanyEmpWage(company, empRatePerHour, numOfWorkingDays, maxHoursPerMonth);
+            this.companyEmpWageList.AddLast(companyEmpWage);
+            this.companyToEmpWageMap.Add(company, companyEmpWage);
+        }
+
+        public void computeEmpWage()
+        {
+            foreach (CompanyEmpWage companyEmpWage in this.companyEmpWageList)
+            {
+                companyEmpWage.setTotalEmpWage(this.computeEmpWage(companyEmpWage));
+                Console.WriteLine(companyEmpWage.toString());
+            }
+        }
+        private int computeEmpWage(CompanyEmpWage companyEmpWage)
+        {
+            int empHrs = 0, totalHr = 0, totalWorkingDays = 0;
+
+            while (totalHr <= companyEmpWage.maxHoursPerMonth && totalWorkingDays < companyEmpWage.numOfWorkingDays)
+            {
+                totalWorkingDays++;
+                Random random = new Random();
+                int empCheck = random.Next(0, 3);
+                switch (empCheck)
+                {
+                    case IS_PART_TIME:
+                        empHrs = 4;
+                        break;
+                    case IS_FUL_TIME:
+                        empHrs = 8;
+                        break;
+                    default:
+                        empHrs = 0;
+                        break;
+                }
+                totalHr += empHrs;
+                Console.WriteLine("Days:  {0} and Employee Hrs: {1}", totalWorkingDays, empHrs);
+
+            }
+            return totalHr * companyEmpWage.empRatePerHour;
+        }
+
+        public int getTotalWage(string company)
+        {
+            return this.companyToEmpWageMap[company].totalEmpWage;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿namespace EmployeeWage
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employeewage program.");
+
+            EmpWageBuilderArray empWageBuilder = new EmpWageBuilderArray();
+
+            empWageBuilder.addCompanyEmpWage("Dmart", 20, 2, 10);
+            empWageBuilder.addCompanyEmpWage("Reliance", 10, 4, 20);
+            empWageBuilder.computeEmpWage();
+            Console.WriteLine("Total Wage for Dmart company :{0} ", empWageBuilder.getTotalWage("Dmart"));
+            Console.WriteLine("Total Wage for Reliance company :{0} ", empWageBuilder.getTotalWage("Reliance"));
+        }
+    }
+}
