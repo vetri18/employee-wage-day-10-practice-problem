3c57f4b7f220040e01e697d66643cb65b5c4ab48
vetri18 <109862568+vetri18@users.noreply.github.com>
2022-08-14 10:09:54 +0530

    uc 10

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿namespace EmployeeWage
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employeewage program.");
+
+            EmpWageBuilderArray empWageBuilder = new EmpWageBuilderArray();
+
+            empWageBuilder.addCompanyEmpWage("Dmart", 20, 2, 10);
+            empWageBuilder.addCompanyEmpWage("Reliance", 10, 4, 20);
+            empWageBuilder.computeEmpWage();
+        }
+    }
+}
