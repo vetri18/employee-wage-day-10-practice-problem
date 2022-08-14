1ab985db3ea15f502d0f06bb91f12f6eb7ee658c
vetri18 <109862568+vetri18@users.noreply.github.com>
2022-08-14 10:04:51 +0530

    using switch method uc 6

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿namespace EmployeeWage
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employeewage program.");
+
+            UsingSwitchMethod usingSwitch = new UsingSwitchMethod();
+            usingSwitch.CheckEmployeeAttendence();
+        }
+    }
+}
