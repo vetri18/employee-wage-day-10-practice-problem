873326ef2241de5cedd70f3c461ec8937e6f2714
vetri18 <109862568+vetri18@users.noreply.github.com>
2022-08-14 10:01:59 +0530

    using switch uc4

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿namespace EmployeeWage
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employeewage program.");
+
+            UsingSwitch usingSwitch = new UsingSwitch();
+            usingSwitch.CheckEmployeeAttendence();
+        }
+    }
+}
