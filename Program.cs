739956aa048a748885ac157aa2e49979318033d5
vetri18 <109862568+vetri18@users.noreply.github.com>
2022-08-14 10:06:07 +0530

    refractor uc 7

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿namespace EmployeeWage
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employeewage program.");
+
+            Refractor usingSwitch = new Refractor();
+            usingSwitch.CheckEmployeeAttendence();
+        }
+    }
+}
---
+++ Refractor.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWage
+{
+    public class Refractor
+    {
+        public void CalculateWages()
+        {
+
+            //Constants
+            const int IS_FULL_TIME = 1;
+            const int IS_PART_TIME = 2;
+            int WAGE_PER_HOUR = 20;
+            int NUM_OF_WORKING_DAYS = 20;
+            //Variables
+            int empHr = 0;
+            int empWage = 0;
+            int totalEmpWage = 0;
+
+            for (int day = 0; day < NUM_OF_WORKING_DAYS; day++)
+            {
+                Random random = new Random();
+                int empCheck = random.Next(3);
+
+                switch (empCheck)
+                {
+                    case IS_PART_TIME:
+                        empHr = 4;
+                        break;
+                    case IS_FULL_TIME:
+                        empHr = 8;
+                        break;
+                    default:
+                        empHr = 0;
+                        break;
+                }
+                empWage = empHr * WAGE_PER_HOUR;
+                totalEmpWage += empWage;
+                Console.WriteLine("Employee wage is: " + empWage);
+            }
+            Console.WriteLine("Employee wage is: " + totalEmpWage);
+        }
+
+        public void CheckEmployeeAttendence()
+        {
+            //Constants
+            int IS_FULL_TIME = 1;
+            int IS_PART_TIME = 2;
+            int WAGE_PER_HOUR = 20;
+            //Variables
+            int empHr = 0;
+            int empWage = 0;
+
+            Random random = new Random();
+            int empCheck = random.Next(3);
+
+            if (empCheck == IS_FULL_TIME)
+            {
+                Console.WriteLine("Employee is Present");
+                empHr = 8;
+            }
+            else if (empCheck == IS_PART_TIME)
+            {
+                Console.WriteLine("Employee did Parttime.");
+                empHr = 4;
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+                empHr = 0;
+            }
+            empWage = empHr * WAGE_PER_HOUR;
+            Console.WriteLine("Employee wage is: " + empWage);
+        }
+    }
+}
