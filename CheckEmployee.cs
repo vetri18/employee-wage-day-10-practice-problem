945a5d84067c027ee0a606d759cdbdb279d863e0
vetri18 <109862568+vetri18@users.noreply.github.com>
2022-08-14 09:53:28 +0530

    check employee uc 1

---
+++ CheckEmployee.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWage
+{
+    public class CheckEmployee
+    {
+        public void CheckEmployeeAttendence()
+        {
+            int IS_FULL_TIME = 1;
+
+            Random random = new Random();
+            int empcheck = random.Next(2);
+
+            if (empcheck == IS_FULL_TIME)
+            {
+                Console.WriteLine("Employee is Present.");
+            }
+            else
+            {
+                Console.WriteLine("Employee is absent.");
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿namespace EmployeeWage
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employeewage program.");
+
+            CheckEmployee checkEmployee = new CheckEmployee();
+            checkEmployee.CheckEmployeeAttendence();
+        }
+    }
+}
